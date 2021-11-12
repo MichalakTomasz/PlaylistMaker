46bd3d27b0e1a0be3cac18423939545b8166b556
Tomasz Michalak <vlad3dracula@gmail.com>
2021-11-12 17:05:40 +0100

    Result class refatoring

--- PlaylistMaker.Commons/Result.cs
+++ PlaylistMaker.Commons/Result.cs
@@ -1,6 +1,4 @@
-﻿using System.Threading.Tasks;
-
-namespace PlaylistMaker.Commons
+﻿namespace PlaylistMaker.Commons
 {
     public static class Result
     {
@@ -8,8 +6,7 @@ namespace PlaylistMaker.Commons
         public static IResult Ok(string message) => new ResultIntern { Message = message, Success = true };
         public static IResult Error() => new ResultIntern { Success = false };
         public static IResult Error(string message) => new ResultIntern { Success = false, Message = message };
-        public static IResult<T> Ok<T>(T arg) => new ResultIntern<T> { Value = arg, Success = true };// (new ResultIntern<T>()).Ok(arg);
-        public static async Task<IResult<T>> OkAsync<T>(Task<T> arg) => new ResultIntern<T> { Value = await arg, Success = true };
+        public static IResult<T> Ok<T>(T arg) => new ResultIntern<T> { Value = arg, Success = true };
         public static IResult<T> Ok<T>(T arg, string message) => new ResultIntern<T> { Value = arg, Success = true, Message = message };
         public static IResult<T> Error<T>(string message) => new ResultIntern<T> { Message = message, Success = false };
         
@@ -17,55 +14,13 @@ namespace PlaylistMaker.Commons
         class ResultIntern : IResult
         {
             public string Message { get;  set; } = "";
-
             public bool Success { get; set; } = false;
-            public IResult Ok()
-            {
-                Success = true;
-                return this;
-            }
-            public IResult Ok(string message)
-            {
-                Success = true;
-                Message = message;
-                return this;
-            }
-
-            public IResult Error(string message)
-            {
-                Success = false;
-                Message = message;
-                return this;
-            }
         }
 
         class ResultIntern<T> : ResultIntern, IResult<T>
         {
             public T Value;
-
             T IResult<T>.Value => Value;
-
-            public IResult<T> Ok(T value)
-            {
-                Success = true;
-                Value = value;
-                return this;
-            }
-
-            public async Task<IResult<T>>OkAsync(Task<T> value)
-            {
-                Success = true;
-                Value = await value;
-                return this;
-            }
-
-            public IResult<T> Error(string message, T value)
-            {
-                Success = false;
-                Message = message;
-                Value = value;
-                return this;
-            }
         }
     }
 
